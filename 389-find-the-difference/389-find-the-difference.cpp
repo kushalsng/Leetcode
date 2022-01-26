7ae003a5071228fa5853cb4d154e77865ccf8b22
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-01-26 14:21:15 +0530

    Time: 4 ms (69.76%), Space: 6.7 MB (25.78%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> prev(26,0), curr(26,0);
-        char res;
-        for(int i =0; i<s.size(); i++){
-            prev[s[i] - 97]++;
-        }
-        for(int i =0; i<t.size(); i++){
-            curr[t[i] - 97]++;
-        }
-        for(int i=0; i<26; i++){
-            if(prev[i] != curr[i]) res = i+97;
-        }
-        return res;
+        int fors=0, fort = 0;
+        for(int i =0 ;i< s.size();i++) fors ^= s[i];
+        for(int i =0 ;i< t.size();i++) fort ^= t[i];
+        
+        return fors ^ fort;
     }
 };
