d5fe93a8682877067f3c33806493837c84153aad
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-01-28 23:52:05 +0530

    Time: 32 ms (7.42%), Space: 12.3 MB (5.27%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    list<int> inorder(TreeNode* root, list<int> &x){
+        if(!root) return x;
+        inorder(root->left, x);
+        x.push_back(root->val);
+        // cout<<x.back()->val<<" ";
+        inorder(root->right, x);
+        return x;
+    }
+    TreeNode* increasingBST(TreeNode* root) {
+        list<int> x;
+        x = inorder(root, x);
+        root->val = x.front();
+        x.pop_front();
+        // root->right = new TreeNode(-1);
+        TreeNode* ptr = root;
+        // cout<<"x.size() = "<<x.size()<<endl;
+        while(!x.empty()){
+            ptr->left = NULL;
+            ptr->right = new TreeNode(x.front());
+            // cout<< x.front()<< " ";
+            x.pop_front();
+            ptr = ptr->right;
+        }
+        ptr = root;
+        while(ptr){
+            cout<<ptr->val<<" ";
+            ptr = ptr->right;
+        }
+        return root;
+    }
+};
