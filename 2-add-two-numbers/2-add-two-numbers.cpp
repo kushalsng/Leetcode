f1c5e3b31b4f60b2a12996b5c26b78654d82cef2
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-03-10 07:08:05 +0530

    Time: 68 ms (23.52%), Space: 71.3 MB (81.70%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,44 +10,52 @@
  */
 class Solution {
 public:
+    
+    //Function to insert a value at end of the list
     ListNode* insertAtEnd(ListNode* l, int val){
         ListNode* n = new ListNode(val);
         if(!l) return n;
         ListNode* temp = l;
-        while(temp->next)temp = temp->next;
+        while(temp->next) temp = temp->next;
         temp->next = n;
         return l;
     }
+    
+    //Function to add remaining list to l3 in case of unequal size
     ListNode* addRemainList(ListNode* l, int* aux, ListNode* l3){
         while(l){
             int sum = l->val + *aux;
-            int uni = sum%10;
+            int uni = sum % 10;
             l3 = insertAtEnd(l3, uni);
-            *aux = sum/10;
+            *aux = sum / 10;
             l = l->next;
         }
         return l3;
     }
+    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* l3 = NULL;
-        int aux =0;
+        int aux = 0;     //aux keeps the carry of sum
         
+        //loop ends if any of the list exhausts
         while(l1 && l2){
-            int sum = l1->val + l2->val + aux;
-            int uni = sum%10;
-            l3 = insertAtEnd(l3, uni);
-            aux = sum/10;
+            int sum = l1->val + l2->val + aux;      // sum var sums both vals and aux
+            int uni = sum % 10;       
+            l3 = insertAtEnd(l3, uni);      //insert only unit place digit of sum var
+            aux = sum / 10;       //aux keeps 10th place digit (if any)
             l1 = l1->next;
             l2 = l2->next;
         }
         
+        //this if block handles situation if any of the list is remaining
         if(l1){
-            l3 = addRemainList(l1, &aux, l3);
+            l3 = addRemainList(l1, &aux, l3);       //sending reference of aux for future use
         }else if(l2){
             l3 = addRemainList(l2, &aux, l3);
         }
         
-        if(aux!=0){
+        //if aux have a value then it is added at the end of the list.
+        if(aux){
             l3 = insertAtEnd(l3, aux);
         }
         return l3;
