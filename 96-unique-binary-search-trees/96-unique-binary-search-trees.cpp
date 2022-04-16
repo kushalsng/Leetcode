860b625ac03e4583e79e0d82ee2c807651592146
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-04-16 10:06:48 +0530

    Time: 3 ms (28.45%), Space: 5.8 MB (88.94%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,11 +1,14 @@
 class Solution {
+    int t[20];
 public:
     int numTrees(int n) {
-        if(n==0)return 1;
-        if(n <= 2) return n;
+        memset(t,-1,sizeof t);
+        t[0] = 1, t[1] = 1, t[2] = 2;
         long sum =0;
         for(int i=1; i<=n; i++){
-            sum += numTrees(i-1) * numTrees(n-i);
+            if(t[i-1] == -1) t[i-1] = numTrees(i-1);
+            if(t[n-i] == -1) t[n-i] = numTrees(n-i);
+            sum += t[i-1]*t[n-i];
         }
         return sum;
     }
