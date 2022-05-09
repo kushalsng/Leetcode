667c919cad17fbf92d8bf5afd8a9d2ec935c4db1
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-05-09 22:00:48 +0530

    Time: 8 ms (8.95%), Space: 6.1 MB (91.33%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i=0;
+        while(i<word.size() && word[i] != ch)i++;
+        if(i<word.size())reverse(word.begin(),word.begin()+i+1);
+        return word;
+    }
+};
