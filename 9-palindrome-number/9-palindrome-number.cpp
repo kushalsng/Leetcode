87af47729281da6caacba75d1c4a792076956a5a
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-05-18 23:57:31 +0530

    Time: 12 ms (80.13%), Space: 6 MB (33.90%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        
+        //negative numbers are not palindrome
         if(x<0) return false;
-        int n = 0, a = x;
+        
+        
+        int n = 0, a = x;       //n stores the number of digits in x
         while(a != 0){
             n++;
             a /= 10;
         }
+        
+        //single digit numbers are palindrome
         if(n == 1) return true;
         
-        int y=0, u=1;
+        int y=0, u=1;           
         int len = n/2;
         
+        //this loop is saving one half of x in y in reverse order
         for(int i = 0; i < len; i++){
             if(i>0) u = 10;
             y= y*u + (x % 10);
             x /= 10;
         }
         
-        if(n%2 !=0) x /= 10;
+        //if number of digits were odd,
+        //we have to remove middle element
+        if(n % 2 != 0) x /= 10;
         
+        //if number is palindrome, x and y should be equal
         return x == y ? true : false;
     }
 };
