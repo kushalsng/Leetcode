e62a8467a6930b9f7a35afbb016a60b59ed864b3
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-05-28 13:08:32 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (26.28%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        // if(n<=2)return *max_element(nums.begin(),nums.end());
+        long long n = nums.size();
         vector<long long> dp(n+1);
-        
-        int i=0;
-        long long ans=LONG_LONG_MIN;
-        while(i<n){
-            if(i<2)dp[i] = nums[i];
-            else if(i>2) dp[i] = nums[i] + max(dp[i-2],dp[i-3]);
-            else dp[i] = nums[i] + dp[i-2];
-            ans = max(dp[i],ans);
-            i++;
+        dp[0] = nums[0];
+        long long ans = nums[0];
+        for(int i=1; i<n; i++){
+            int a = nums[i], b = dp[i-1];
+            if(i>1)a += dp[i-2];
+            dp[i] = max(a,b);
+            ans = max(ans, dp[i]);
         }
         return ans;
     }
