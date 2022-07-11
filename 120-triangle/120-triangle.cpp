17117a132209d1fc5e7d741a4b0ddf6d2f89cfcf
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-07-11 13:16:19 +0530

    Time: 3 ms (97.93%), Space: 8.9 MB (36.46%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,25 +3,19 @@ public:
     
     int minimumTotal(vector<vector<int>>& tri) {
         int m = tri.size();
-        vector<vector<int>> dp(m,vector<int>(m,-1));
+        vector<int> dp(m);
         for(int j= 0; j<m;j++){
-            dp[m-1][j] = tri[m-1][j];
+            dp[j] = tri[m-1][j];
         }
         for(int i= m-2; i>=0; i--){
+            vector<int> temp(m);
             for(int j=i;j>=0; j--){
-                
-                int b = dp[i+1][j];
-                int d = dp[i+1][j+1];
-                dp[i][j] = min(b,d) + tri[i][j];
+                int b = dp[j];
+                int d = dp[j+1];
+                temp[j] = min(b,d) + tri[i][j];
             }
+            dp = temp;
         }
-        // for(int i=0; i<m; i++){
-        //     for(int j=0;j<m; j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return dp[0][0];
-        // return dp[m-1][n-1];
+        return dp[0];
     }
 };
