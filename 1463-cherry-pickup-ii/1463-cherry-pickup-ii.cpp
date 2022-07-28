ae72a2c854c16ed015ed075770178c08e4e518eb
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-07-28 11:03:38 +0530

    Time: 325 ms (9.37%), Space: 9.3 MB (92.97%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
     int m,n;
-    int f(int i, int j1, int j2, vector<vector<int>> grid, vector<vector<vector<int>>> &dp){
-        if(j1<0 || j1>=n || j2<0 || j2>=n || i<0) return -1e8;
-        if(i == m-1){
-            // return 0;
-            if(j1 == j2)return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
-        }
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-        int mx = -1e8;
-        for(int x = -1; x<=1; x++){
-            for(int y= -1; y<=1; y++){
-                int val =0;
-                if(j1 == j2) val = grid[i][j1];
-                else val = grid[i][j1] + grid[i][j2];
-                val += f(i+1, j1+x, j2+y, grid, dp);
-                mx = max(mx, val);
-            }
-        }
-        dp[i][j1][j2] = mx;
-        return mx;
-    }
     int cherryPickup(vector<vector<int>>& grid) {
         m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n,0)));
-        
+        vector<vector<int>> front(n, vector<int>(n,0));
+        vector<vector<int>> cur(n, vector<int>(n,0));
         for(int j1 =0; j1<n;j1++){
             for(int j2 = 0; j2<n; j2++){
-                if(j1 == j2) dp[m-1][j1][j2] = grid[m-1][j1];
-                else dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+                if(j1 == j2) front[j1][j2] = grid[m-1][j1];
+                else front[j1][j2] = grid[m-1][j1] + grid[m-1][j2];
             }
         }
 
@@ -43,16 +22,17 @@ public:
                             if(j1 == j2) val = grid[i][j1];
                             else val = grid[i][j1] + grid[i][j2];
                             if(j1+x>=0 && j1+x<n && j2+y>=0 && j2+y<n)
-                                val += dp[i+1][j1+x][j2+y];
+                                val += front[j1+x][j2+y];
                             else val += -1e8;
                             
                             mx = max(mx, val);
                         }
                     }
-                    dp[i][j1][j2] = mx;
+                    cur[j1][j2] = mx;
                 }
             }
+            front = cur;
         }
-        return dp[0][0][n-1];
+        return cur[0][n-1];
     }
 };
