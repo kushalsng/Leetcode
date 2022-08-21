70471c5de98a7fd2b4abe34e9cbe3357cf74be36
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-08-21 09:39:33 +0530

    Time: 54 ms (100.00%), Space: 13.1 MB (100.00%) - LeetHub

---
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string largestPalindromic(string num) {
+        map<char,int>mp;
+        for(auto c: num)mp[c]++;
+        if(mp.size() == 1 && num[0] == '0') return "0";
+        int odd = -1;
+        for(auto it = mp.begin(); it != mp.end(); it++){
+            if(it->second%2)
+                odd = max(odd, it->second);
+        }
+        char c = (char)0;
+        // if(odd != -1){
+            for(auto it = mp.begin(); it != mp.end(); it++){
+                if(it->second == 1) c = max(c,it->first);
+            }
+        // }
+        string ans = "";
+        for(auto it = mp.rbegin(); it != mp.rend(); it++){
+                if(it->second %2 == 0){
+                    if(ans == "" && it->first == '0')continue;
+                    else{
+                        int fx = it->second/2;
+                        while(it->second>fx){
+                            ans += it->first;
+                            it->second--;
+                        }
+                    }
+                }else{
+                    c = max(c,it->first);
+                    it->second--;
+                    if(ans == "" && it->first == '0')continue;
+                    else{
+                        int fx = it->second/2;
+                        while(it->second>fx){
+                            ans += it->first;
+                            it->second--;
+                        }
+                    }
+                }
+            }
+        int ind = ans.size()-1;
+        if(c != 0){
+            ans += c;
+        }
+        while(ind>=0){
+            ans += ans[ind--];
+        }
+        return ans;
+    }
+};
