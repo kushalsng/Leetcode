7f527b00e32d1e6f2033b27d22d9ef5c09ca84d7
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-09-13 10:58:56 +0530

    Time: 131 ms (63.59%), Space: 19.8 MB (95.65%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& a) {
         vector<vector<int>> ans;
-        if(a.size()<3)return ans;
+        int n = a.size();
+        if(n<3)return ans;
         sort(a.begin(), a.end());
         for(int i=0; i<a.size()-2; i++){
-            int l = i+1, r=a.size()-1;
-            if(i==0 || (a[i] != a[i-1])){
+            int l = i+1, r = n-1;
+            if(i == 0 || a[i] !=a[i-1]){
                 while(l<r){
-                    // cout<<l<<" "<<r<<endl;
-                    if(a[l] + a[r] == -a[i]){
-                        ans.push_back({a[i],a[l++],a[r--]});
-                        while(l<r && a[l] == a[l-1]) l++;
-                        while(l<r && a[r] == a[r+1]) r--;
-                    }else if(-a[i] <a[l]+a[r])r--;
+                    if(a[l] +a[r] == -a[i]){
+                        ans.push_back({a[l++], a[r--], a[i]});
+                        while(l<r and a[l] == a[l-1])l++;
+                        while(l<r and a[r] == a[r+1])r--;
+                    }else if(-a[i] < a[l] + a[r]) r--;
                     else l++;
                 }
             }
+            
         }
         return ans;
     }
