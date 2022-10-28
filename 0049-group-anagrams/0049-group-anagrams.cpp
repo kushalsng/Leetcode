64ae1bd8c8a2d1c10c4a023cd2adcc7d0c990f97
Kushal Singh <59888324+kushalsng@users.noreply.github.com>
2022-10-28 16:44:06 +0530

    Time: 83 ms (40.14%), Space: 19.6 MB (84.38%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp;
+        for(auto c: s)mp[c]++;
+        for(auto c: t){
+            if(mp[c] == 0) return false;
+            mp[c]--;
+        }
+        for(auto it = mp.begin();it != mp.end(); it++){
+            if(it->second != 0) return false;
+        }
+        return true;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        
+        for(int i=0; i<strs.size(); i++){
+            string save = strs[i];
+            sort(strs[i].begin(),strs[i].end());
+            // cout<<strs[i]<<" "<<save<<", ";
+            mp[strs[i]].push_back(save);
+        }
+        for(auto it = mp.begin(); it != mp.end(); it++){
+            ans.push_back(it->second);
+        }
+        return ans;
+    }
+};
